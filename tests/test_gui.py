c6d33623a4bc35f016c240b12e7a9eca5501389b
Matthew Brett <matthew.brett@gmail.com>
2025-05-21 21:40:56 +0100

    Adapt GUI test.

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,5 +1,6 @@
 print("Running imports")
 import faulthandler
+from platform import system
 
 faulthandler.enable()
 
@@ -7,12 +8,17 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 
+plat2backend = {
+    'Darwin': 'NSView',
+    'Windows': 'TkAgg',
+    'Linux': 'TkAgg'
+}
 
 this_path = Path(__file__).parent
 
 # Matplotlib
 print("Running matplotlib tests")
 fig, _ = plt.subplots()
-want = "QTAgg"
+want = plat2backend[system()]
 assert want in repr(fig.canvas), repr(fig.canvas)
 plt.close("all")
