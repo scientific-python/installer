91f21eaea8f0656351d88a1d716354cac991d866
Matthew Brett <matthew.brett@gmail.com>
2025-05-23 20:37:30 +0100

    Set Jupyter paths to avoid user's setup.

--- sp-installer-menu/menu/open_prompt.sh
+++ sp-installer-menu/menu/open_prompt.sh
@@ -5,7 +5,9 @@
 if [[ -f ~/.bashrc ]] && [[ ${OSTYPE} != 'darwin'* ]]; then
     source ~/.bashrc
 fi
-source #PREFIX#/bin/activate base
+source "#PREFIX#/bin/activate" base
+export JUPYTER_CONFIG_DIR="#PREFIX#/etc/jupyter"
+export JUPYTER_DATA_DIR="#PREFIX#/share/jupyter"
 echo "Using $(python --version) from $(which python)"
 echo "This is Scientific Python version #PKG_VERSION#"
 workdir="$HOME/Documents/scientific-python"
