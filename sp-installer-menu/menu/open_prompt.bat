1c3bb07f86dfad042e6cb77116d3e9e774555ea9
Matthew Brett <matthew.brett@gmail.com>
2025-05-28 10:59:47 +0100

    Workaround for conda activation error.

--- sp-installer-menu/menu/open_prompt.bat
+++ sp-installer-menu/menu/open_prompt.bat
@@ -1,7 +1,10 @@
 :: This is used to initialize the bash prompt on Windows.
 @ECHO OFF
 
-call #PREFIX#\Scripts\Activate.bat base
+:: Workaround for
+:: https://github.com/scientific-python/installer/pull/3#issuecomment-2907866303
+set "CONDA_EXE=#PREFIX#\Scripts\conda.exe"
+call "#PREFIX#\Scripts\Activate.bat" base
 FOR /F "tokens=*" %%g IN ('python --version') do (SET PYVER=%%g)
 FOR /F "tokens=*" %%g IN ('where python') do (SET PYPATH=%%g)
 
